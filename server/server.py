be0aefa791a7778f7c2d1f0f53ece029f0a7ffea
Anthony Vento <anthony.vento@utexas.edu>
2019-11-15 23:08:13 -0600

    added preliminary server for debug

---
+++ server/server.py
@@ -0,0 +1,41 @@
+from stableconfigs import StableConfig
+from flask import Flask, jsonify, request
+import os
+
+app = Flask(__name__)
+
+# Based expected call/response output of Post to look like Figure 6 from Proj. Definition
+@app.route("/", methods=['POST'])
+def my_post():
+        received_json = request.get_json()
+        monomers = received_json['monomers']
+
+        # Creates a file to write monomers in order to call get_stable_config
+        my_file = "temp.txt"
+        f = open(my_file, "w+")
+        for index, monomer in enumerate(monomers):
+            f.write(' '.join(monomer))
+            if index != len(monomers) - 1:
+                f.write('\n')
+        f.close()
+
+        # Call get stable config and delete file
+        polymers = StableConfig.get_stable_config(my_file)
+        os.remove(my_file)
+
+        # Return polymer output in expected format
+        polymer_output = []
+        for index, polymer in enumerate(polymers):
+            cur_polymer = []
+            for monomer in polymer.monomer_list:
+                cur_monomer = list(map(lambda x: (x.name + "*") if x.IsComplement else x.name, monomer.BindingSites))
+                cur_polymer.append(cur_monomer)
+            polymer_output.append(cur_polymer)
+
+        return jsonify({"polymers": polymer_output}), 201
+
+
+def run_app():
+    app.run(debug=True)
+
+
--- stableconfigs/__main__.py
+++ stableconfigs/__main__.py
@@ -1,8 +1,13 @@
 from stableconfigs import StableConfig
+from server import server
 import sys
 
 if __name__ == '__main__':
     # python3 -m src /home/kyle/StableConfigs/input/wraparound_sorting_network.txt
-    
-    file_path = sys.argv[1]
-    StableConfig.get_stable_config(file_path)
+    API_Debug = True
+    if not API_Debug:
+        file_path = sys.argv[1]
+        StableConfig.get_stable_config(file_path)
+    else:
+        server.run_app()
+
