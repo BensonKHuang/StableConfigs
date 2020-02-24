8704256865cf8acaa10a44ed032c1ac52312efe5
kyle <kyle.a.zhou@gmail.com>
2020-02-24 09:57:48 -0600

    testApi calling live server

--- testApi.py
+++ testApi.py
@@ -2,5 +2,5 @@ import requests
 my_mon = [['a', 'b'], ['a*'], ['b*'], ['a*', 'b*']]
 
 dicToSend = {'monomers': my_mon}
-res = requests.post('http://localhost:5000/', json=dicToSend)
+res = requests.post('http://198.23.133.106:5005/', json=dicToSend)
 print(res.json())
